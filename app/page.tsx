4e3b204dcf276a9f1928f494fca17a546e75fb3f
Stanislav Efimtsev <evendot@yandex.ru>
2024-08-21 14:34:41 +0300

    Added sort popup UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Categories, Container, Title } from "@/components/shared";
+import { Categories, Container, SortPopup, Title } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -6,7 +6,8 @@ export default function Home() {
     <Container className="mt-8">
       <Title text="Всё снаряжение" size="lg" className="font-extrabold">
       </Title>
-      <Categories></Categories>
+      <Categories/>
+      <SortPopup/>
     </Container>
     </>
   );
